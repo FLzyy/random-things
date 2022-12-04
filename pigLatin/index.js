05422e1c919801ccfa8ef9a659133c4aefe42879
FLzyy <flzyy72@gmail.com>
2022-12-04 10:37:26 +0800

    opti

--- pigLatin/index.js
+++ pigLatin/index.js
@@ -1,4 +1,4 @@
 const word = "happy";
 const firstLetter = word.charAt(0).toLowerCase();
 
-console.log(`${ word.slice(1, word.length) }-${firstLetter}ay`);
+console.log(`${word.slice(1)}-${firstLetter}ay`);
